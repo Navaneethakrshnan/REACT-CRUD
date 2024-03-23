43ed6deb564a5b3e4056f78bd1552e04bdd2fabf
Navaneethakrshnan <navaneethak381@gmail.com>
2024-03-23 19:08:58 +0530

    create code

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Create() {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3000/users", values)
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div>Create</div>
-  )
+    <div
+      className="d-flex w-100 vh-50 justify-content-center align-item-center bg-white"
+      style={{ marginTop: "10%" }}
+    >
+      <div className="w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
+        <h1>Add New User</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-2">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              placeholder="Enter name"
+              onChange={(e) => setValues({ ...values, name: e.target.value })}
+            ></input>
+          </div>
+          <div className="mb-2 ">
+            {" "}
+            <label htmlFor="name">Email:</label>
+            <input
+              type="text"
+              name="email"
+              className="form-control"
+              placeholder="Enter email"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
+            ></input>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="name">Phone:</label>
+            <input
+              type="text"
+              name="phone"
+              className="form-control"
+              placeholder="Enter Phone"
+              onChange={(e) => setValues({ ...values, phone: e.target.value })}
+            ></input>
+          </div>
+          <button className="btn btn-success me-3">Submit</button>
+          <Link to="/" className="btn btn-primary">
+            Back
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Create
+export default Create;
