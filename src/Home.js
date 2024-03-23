df3fe6e5abbe5250253396df8a9b371aba72f624
Navaneethakrshnan <navaneethak381@gmail.com>
2024-03-23 16:48:47 +0530

    Home page designs

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [data, setData] = useState([]);
@@ -7,30 +8,52 @@ function Home() {
   useEffect(() => {
     axios
       .get("http://localhost:3000/users")
-      .then((res) => console.log(res.data))
+      .then((res) => setData(res.data))
       .catch((err) => console.log(err));
   }, []);
   return (
     <div className="d-flex flex-column justify-content-center align-items-center bg-light vh-100 ">
       <h1>List of Users</h1>
-      <div className="W-75 rounded bg-white border shadow p-3">
-        <table>
+      <div
+        className="w-75 rounded border shadow p-3"
+        style={{ backgroundColor: "#fff" }}
+      >
+        <div className="d-flex justify-content-end">
+          <Link
+            to="/create"
+            className="btn"
+            style={{ backgroundColor: "#637A9F", color: "#fff" }}
+          >
+            Add ➕
+          </Link>
+        </div>
+        <table className="table table-striped rounded-2 p-4">
           <thead>
             <tr>
-              <th>ID</th>
-              <th>NAME</th>
-              <th>EMAIL</th>
+              <th className="g-col-6">ID</th>
+              <th className="g-col-6">NAME</th>
+              <th className="g-col-6">EMAIL</th>
               <th>PHONE</th>
               <th>ACTION</th>
             </tr>
           </thead>
           <tbody>
             {data.map((d, i) => (
-              <tr key={id}>
-                <td>{(d, id)}</td>
-                <td>{(d, name)}</td>
-                <td>{(d, email)}</td>
-                <td>{(d, phone)}</td>
+              <tr key={i}>
+                <td>{d.id}</td>
+                <td>{d.name}</td>
+                <td>{d.email}</td>
+                <td>{d.phone}</td>
+                <td>
+                  <button
+                    className="btn btn-sm me-3"
+                    style={{ backgroundColor: "#fcd34d" }}
+                  >
+                    Read
+                  </button>
+                  <button className="btn btn-sm btn-dark me-3">Edit</button>
+                  <button className="btn btn-sm btn-danger">Delete</button>
+                </td>
               </tr>
             ))}
           </tbody>
